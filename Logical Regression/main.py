788d800d417578ea069931f984a6e91a0f49e5bf
ryanhogan <ryanhogan284@gmail.com>
2023-08-31 00:44:15 +1000

    Added Datasets

---
+++ Logical Regression/main.py
@@ -0,0 +1,29 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+from six.moves import urllib
+
+import tensorflow.compat.v2.feature_column as fc
+import tensorflow as tf
+
+dfTrain = pd.read_csv('train.csv')
+dfTest = pd.read_csv('eval.csv')
+
+y_train = dfTrain.pop('survived')
+y_test = dfTest.pop('survived')
+
+CATEGORICAL_COLUMNS = ['sex', 'n_siblings_spouses', 'parch', 'class', 'deck', 'embark_town', 'alone']
+NUMERIC_COLUMNS = ['age', 'fare']
+
+feature_columns = []
+for feature_name in CATEGORICAL_COLUMNS:
+    vocabulary = dfTrain[feature_name].unique()
+    feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
+
+for feature_name in NUMERIC_COLUMNS:
+    feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
+
+print(feature_columns)
--- main.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-
-t = tf.zeros([5, 5, 5, 5])
-t = tf.reshape(t, [625])
-
-print(t)
